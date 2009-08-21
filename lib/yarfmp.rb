5cf8aea758875eaa3a318821b5c8da9ceabb45ac
Jon Moses <jon@burningbush.us>
2009-08-21 13:18:46 -0400

    Initial checkin

---
+++ lib/yarfmp.rb
@@ -0,0 +1,88 @@
+module YARFMP
+  
+  def self.included( base )
+    base.class_eval do
+      include InstanceMethods
+      
+      helper_method :has_flash_messages?
+    end
+  end
+
+  module InstanceMethods
+    FLASH_MESSAGE_LEVELS = [:error, :notice]
+    FLASH_MESSAGE_KEY = :flash_messages
+
+    def add_message( str )
+      add_flash_message(:notice, str)
+    end
+    
+    def add_error( str )
+      add_flash_message(:error, str)
+    end
+    
+    protected
+    
+      def add_flash_message( level, str )
+        return nil unless FLASH_MESSAGE_LEVELS.include?(level)
+        
+        logger.debug("#{self.class.name}:: Adding #{level} message: #{str}")
+        
+        existing = (flash[FLASH_MESSAGE_KEY] ||= {})[level]
+        if existing and existing.is_a?(String)
+          existing = [existing, str].flatten
+        elsif existing and existing.is_a?(Array)
+          existing << str
+        elsif ! existing
+          existing = str
+        end
+        
+        flash[FLASH_MESSAGE_KEY][level] = existing
+      end
+      
+      def has_flash_messages?
+        flash[FLASH_MESSAGE_KEY] and ! flash[FLASH_MESSAGE_KEY].empty?
+      end
+      
+  end
+  
+  module HelperMethods
+    def render_flash_messages
+      unless has_flash_messages?
+        return content_tag(:div, "", :id => "flash_messages_wrap") 
+      end
+      
+      content_tag(:div, :id => "flash_messages_wrap") do 
+        content_tag(:div, :id => "flash_messages") do
+          YARFMP::InstanceMethods::FLASH_MESSAGE_LEVELS.collect do |level|
+            render_flash_message_single(flash[YARFMP::InstanceMethods::FLASH_MESSAGE_KEY][level], :class => "message_#{level}")
+          end.join("\n")
+        end
+      end
+    end
+    
+    def render_flash_message_single( messages, options = {} )
+      return "" unless messages
+      
+      if messages.is_a?(Array)
+        content_tag(:ul, options) do
+          messages.collect do |msg|
+            content_tag(:li, escape_single(msg))
+          end.join("\n")
+        end
+      else
+        content_tag(:div, options) do
+          escape_single messages
+        end
+      end
+    end
+    
+    def escape_single( message )
+      h(message).gsub("\n", "<br/>")
+    end
+    
+  end
+end
+
+
+ActionController::Base.send(:include, YARFMP )
+ActionView::Base.send(:include, YARFMP::InstanceMethods ) ## This only works once!?????
---
+++ test/yarfmp_test.rb
@@ -0,0 +1,125 @@
+# # These don't work yet.
+# require 'rubygems'
+# require 'action_controller'
+# require 'active_support'
+# require 'active_support/test_case'
+# require 'shoulda'
+# require File.join( File.dirname(__FILE__), '..', 'lib', 'yarfmp.rb')
+# 
+# class FakeMessaging < ActionController::Base
+#   include ActionView::Helpers::TagHelper
+#   include ActionView::Helpers::SanitizeHelper
+#   include ActionView::Helpers::CaptureHelper
+#   
+#   include YARFMP
+#   include YARFMP::HelperMethods
+#   
+#   
+#   def flash
+#     @flash ||= {}
+#   end
+#   
+#   def output_buffer=( buf )
+#     @output_buffer = buf
+#   end
+#   
+#   def output_buffer
+#     @output_buffer
+#   end
+#   
+#   public :h
+# end
+# 
+# class YARFMPTest < ActiveSupport::TestCase
+#   context "flash messaging" do
+#     setup do
+#       @helper = FakeMessaging.new
+#     end
+#     
+#     context "with no messages" do
+#       should "render an empty string" do
+#         assert_equal %Q{<div id="flash_messages_wrap"></div>}, @helper.render_flash_messages 
+#       end
+#     end
+#     
+#     context "with a single message" do
+#       context "just plain text" do
+#         setup do
+#           @message = "Testing 1 2 3"
+#           @helper.add_message(@message)
+#         end
+#         
+#         should "render a list with the message as an li" do
+#           assert_single_message @message, @helper.render_flash_messages
+#         end        
+#       end
+#       
+#       context "with plain text and line breaks" do
+#         setup do
+#           @message = "First line\n2nd line"
+#           @helper.add_message( @message )
+#         end
+#         
+#         should "convert the line break into a html break" do
+#           assert_single_message @message.gsub("\n", "<br/>"), @helper.render_flash_messages
+#         end
+#       end
+#       
+#       context "with html" do
+#         setup do
+#           @message = "<h1>TEST ME!</h1>"
+#           @helper.add_message(@message)
+#         end
+#         
+#         should "have the html escaped" do
+#           assert_single_message @helper.h(@message), @helper.render_flash_messages
+#         end
+#         
+#       end
+#     end
+#     
+#     context "with multiple messages" do
+#       context "just plain text" do
+#         setup do
+#           @messages = [
+#             'message 1',
+#             'message 2',
+#           ]
+#           
+#           @messages.each {|m| @helper.add_message(m) }
+#         end
+#         
+#         should "render the messages as a list" do
+#           assert_multiple_messages @messages, @helper.render_flash_messages
+#         end
+#       end
+#       
+#       context "with html" do
+#         setup do
+#           @messages = [
+#             "<h1>Test message</h1>",
+#             "<span>Blerg</span>",
+#           ]
+#           
+#           @messages.each {|m| @helper.add_message(m) }
+#         end
+#         
+#         should "render the messages as a list, that's escaped" do
+#           assert_multiple_messages @messages.collect {|m| @helper.h(m) }, @helper.render_flash_messages
+#         end
+#       end
+#     end
+#   end
+#   
+#   def assert_single_message( message, output, level = :notice )
+#     assert_equal %Q{<div id="flash_messages_wrap"><div id="flash_messages">\n<div class="message_#{level.to_s}">#{message}</div></div></div>}, output
+#   end
+#   
+#   def assert_multiple_messages( messages, output, level = :notice )
+#     expected_str = %Q{<div id="flash_messages_wrap"><div id="flash_messages">\n<ul class="message_#{level.to_s}">}
+#     expected_str << messages.collect {|m| %Q{<li>#{m}</li>} }.join("\n")
+#     expected_str << "</ul></div></div>"
+#     
+#     assert_equal expected_str, output
+#   end
+# end
